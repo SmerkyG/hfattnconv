b12a199cb812dbcc3752ff3b9e0bb4c556e78501
SmerkyG <SmerkyG@users.noreply.github.com>
2024-11-21 22:07:14 -0500

    allow conversion from safetensors to sharded safetensors

--- convert_to_safetensors.py
+++ convert_to_safetensors.py
@@ -1,6 +1,6 @@
 import sys
 import torch
-from safetensors.torch import save_file
+from safetensors.torch import save_file, load_file
 from huggingface_hub import save_torch_state_dict
 
 if len(sys.argv) != 3:
@@ -12,7 +12,11 @@ in_model_path = sys.argv[1]
 out_model_path = sys.argv[2]
 
 print("Loading file...")
-state_dict = torch.load(in_model_path, map_location='cpu', weights_only=True)
+if in_model_path.lower().endswith('.safetensors'):
+    state_dict = load_file(in_model_path, device='cpu')
+else:
+    state_dict = torch.load(in_model_path, map_location='cpu', weights_only=True)
+
 if out_model_path.endswith('.safetensors'):
     print("Saving file...")
     save_file(state_dict, out_model_path, metadata=dict(format='pt'))
